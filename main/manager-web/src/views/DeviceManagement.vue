f3fcf2fedf859f156483cbcacbe4588f8d773a4a
zhuoqinglian <1035449612@qq.com>
2026-06-03 14:40:01 +0800

    feat: mac地址添加马赛克功能

--- main/manager-web/src/views/DeviceManagement.vue
+++ main/manager-web/src/views/DeviceManagement.vue
@@ -31,7 +31,11 @@
               </el-table-column>
               <el-table-column :label="$t('device.firmwareVersion')" prop="firmwareVersion"
                 align="center"></el-table-column>
-              <el-table-column :label="$t('device.macAddress')" prop="macAddress" align="center"></el-table-column>
+              <el-table-column :label="$t('device.macAddress')" prop="macAddress" align="center">
+                <template slot-scope="scope">
+                  <MacAddressMask :macAddress="scope.row.macAddress" />
+                </template>
+              </el-table-column>
               <el-table-column :label="$t('device.bindTime')" prop="bindTime" align="center"></el-table-column>
               <el-table-column :label="$t('device.lastConversation')" prop="lastConversation"
                 align="center"></el-table-column>
@@ -131,13 +135,15 @@ import AddDeviceDialog from "@/components/AddDeviceDialog.vue";
 import HeaderBar from "@/components/HeaderBar.vue";
 import ManualAddDeviceDialog from "@/components/ManualAddDeviceDialog.vue";
 import VersionFooter from "@/components/VersionFooter.vue";
+import MacAddressMask from "@/components/MacAddressMask.vue";
 
 export default {
   components: {
     HeaderBar,
     AddDeviceDialog,
     ManualAddDeviceDialog,
-    VersionFooter
+    VersionFooter,
+    MacAddressMask,
   },
   data() {
     return {
